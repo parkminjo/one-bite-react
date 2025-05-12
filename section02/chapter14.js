c7e06ccf0ecaba22f8472443f7a5097309acfe6b
parkminjo <cruiser367@naver.com>
2025-05-12 13:31:27 +0900

    chore: async, await 학습 완료

---
+++ section02/chapter14.js
@@ -0,0 +1,26 @@
+// async
+// 어떤 함수를 비동기 함수로 만들어주는 키워드
+// 함수가 promise를 반환하도록 변환해주는 키워드
+
+async function getData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        name: '박민조',
+        id: 'cruiser367',
+      });
+    }, 1000);
+  });
+}
+
+// console.log(getData());
+
+// await
+// async 함수 내부에서만 사용이 가능한 키워드
+// 비동기 함수가 다 처리되기를 기다리는 역할
+async function printData() {
+  const data = await getData();
+  console.log(data);
+}
+
+printData();
