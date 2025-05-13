ac7f81fe490ef70322502736e1c890ca3fdf573e
parkminjo <cruiser367@naver.com>
2025-05-13 11:32:10 +0900

    comment: 주석 추가

--- section04/src/main.tsx
+++ section04/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+// root라는 아이디를 가진 요소를 불러옴
+// render 메서드를 통해 App 컴포넌트를 렌더링하고 있음
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
