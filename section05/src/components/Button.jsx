047032c8cd9564f69ee1d534d5a6060a135c5a77
parkminjo <cruiser367@naver.com>
2025-05-16 22:42:01 +0900

    chore: 이벤트 핸들링 학습 완료

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,7 +1,13 @@
 const Button = ({ text, color = 'black', children }) => {
-  console.log(text);
+  // 이벤트 객체
+  // 합성 이벤트 객체 = 모든 웹 브라우저의 이벤트 객체를 하나로 통일한 형태
+  const handleClick = (event) => {
+    console.log(event);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color }}>
+    <button onClick={handleClick} style={{ color }}>
       {text} - {color.toUpperCase()}
       {children}
     </button>
