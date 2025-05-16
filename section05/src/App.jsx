ae3c133a92ff2c8bbf89bdfed4498d092ce4f728
parkminjo <cruiser367@naver.com>
2025-05-16 23:14:10 +0900

    chore: 리렌더링 발생하는 상황 학습 완료

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,24 +1,15 @@
 import './App.css';
-import { useState } from 'react';
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
 
 const App = () => {
   // state 초기값
   // state를 변화시키는 함수
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState('OFF');
 
   return (
     <div>
-      <div>
-        <h1>{light}</h1>
-        <button onClick={() => setLight(light === 'ON' ? 'OFF' : 'ON')}>
-          {light === 'ON' ? '끄기' : '켜기'}
-        </button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-      </div>
+      <Bulb />
+      <Counter />
     </div>
   );
 };
