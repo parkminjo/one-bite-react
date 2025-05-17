b3ccb14b7eabda7b038bdbed85e84f2228e268a3
parkminjo <cruiser367@naver.com>
2025-05-17 14:46:06 +0900

    chore: 초기 세팅

---
+++ section07/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState } from 'react';
+import Controller from './components/Controller';
+import Viewer from './components/Viewer';
+
+const App = () => {
+  // 왜 App 컴포넌트에서 상태를 관리할까?
+  // Viewer 컴포넌트와 Controller 컴포넌트는 형제 관계이기 때문에 서로 값을 공유할 수 없는 구조임
+  const [count, setCount] = useState(0);
+
+  return (
+    <main className="app">
+      <h1>Simple Counter</h1>
+      <Viewer count={count} />
+      <Controller setCount={setCount} />
+    </main>
+  );
+};
+
+export default App;
---
+++ section07/src/components/Viewer.jsx
@@ -0,0 +1,10 @@
+const Viewer = ({ count }) => {
+  return (
+    <section>
+      <div>현재 카운트:</div>
+      <h1>{count}</h1>
+    </section>
+  );
+};
+
+export default Viewer;
