1d545c04977bcbb200c9f3a7dd16bd94865d89c2
parkminjo <cruiser367@naver.com>
2025-05-17 13:57:14 +0900

    comment: app 컴포넌트에 주석 추가

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -4,6 +4,8 @@ import Controller from './components/Controller';
 import Viewer from './components/Viewer';
 
 const App = () => {
+  // 왜 App 컴포넌트에서 상태를 관리할까?
+  // Viewer 컴포넌트와 Controller 컴포넌트는 형제 관계이기 때문에 서로 값을 공유할 수 없는 구조임
   const [count, setCount] = useState(0);
 
   return (
