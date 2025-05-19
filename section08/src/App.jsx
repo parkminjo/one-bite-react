bda35d51d866217b5902385d516eaff7c0d3fdf1
parkminjo <cruiser367@naver.com>
2025-05-19 13:16:38 +0900

    feat: todo 추가 기능 구현 완료

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,15 +2,52 @@ import './App.css';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import TodoList from './components/TodoList';
+import { useRef, useState } from 'react';
 
 const App = () => {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const addTodo = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content,
+      date: new Date().getTime(),
+    };
+
+    setTodos((prev) => [...prev, newTodo]);
+  };
+
   return (
     <div className="app">
       <Header />
-      <Editor />
+      <Editor onCreate={addTodo} />
       <TodoList />
     </div>
   );
 };
 
 export default App;
+
+// TodoList 데이터 모델링
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: 'Next.js 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    isDone: false,
+    content: 'Computer Science 공부하기',
+    date: new Date().getTime(),
+  },
+];
--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,40 @@
 import './Editor.css';
+import { useRef, useState } from 'react';
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState('');
+  const contentRef = useRef();
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (content === '') {
+      contentRef.current.focus();
+      alert('TODO를 입력해주세요.');
+      return;
+    }
+
+    onCreate(content);
+    setContent('');
+  };
+
+  const handleKeydown = (event) => {
+    if (event.code === 'Enter') handleSubmit();
+  };
+
   return (
     <div className="editor">
-      <input type="text" placeholder="새로운 TODO를 입력하세요" />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        type="text"
+        placeholder="새로운 TODO를 입력하세요"
+        value={content}
+        onChange={handleChange}
+        onKeyDown={handleKeydown}
+      />
+      <button onClick={handleSubmit}>추가</button>
     </div>
   );
 };
