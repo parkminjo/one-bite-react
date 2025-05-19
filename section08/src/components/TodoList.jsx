4d5a64ed2e48a201d2ac2ebc5bae25d5493cdc12
parkminjo <cruiser367@naver.com>
2025-05-19 13:55:27 +0900

    feat: todo list 검색 기능 구현 완료

--- section08/src/components/TodoList.jsx
+++ section08/src/components/TodoList.jsx
@@ -1,13 +1,38 @@
+import { useState } from 'react';
 import TodoItem from './TodoItem';
 import './TodoList.css';
 
 const TodoList = ({ todos }) => {
+  const [search, setSearch] = useState('');
+
+  const handleChangeSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === '') {
+      return todos;
+    }
+
+    const filteredData = todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+    return filteredData;
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="todo-list">
       <h4>Todo List 🌱</h4>
-      <input type="text" placeholder="검색어를 입력하세요" />
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요"
+        value={search}
+        onChange={handleChangeSearch}
+      />
       <ul className="todo-list-wrapper">
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           return <TodoItem key={todo.id} {...todo} />;
         })}
       </ul>
