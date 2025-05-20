acc887aaf905a9fc627c3ecc4fd6164a6bade098
parkminjo <cruiser367@naver.com>
2025-05-20 11:49:21 +0900

    chore: 페이지 라우팅 설정 완료

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import NotFound from './pages/NotFound';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
