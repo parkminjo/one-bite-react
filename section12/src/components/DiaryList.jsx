a3136443fb9d59fc54c4969d178f40bff81aafb4
parkminjo <cruiser367@naver.com>
2025-05-21 11:29:29 +0900

    feat: 최신 순/오래된 순 필터링 구현 완료

--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -2,9 +2,27 @@ import './DiaryList.css';
 import DiaryItem from './DiaryItem';
 import Button from './Button';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const DiaryList = ({ monthlyDiaryList }) => {
   const navigate = useNavigate();
+  const [sortType, setSortType] = useState();
+
+  const onChangeSortType = (event) => {
+    setSortType(event.target.value);
+  };
+
+  const getSortedData = () => {
+    return monthlyDiaryList.toSorted((a, b) => {
+      if (sortType === 'oldest') {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+
+  const sortedDiaryList = getSortedData();
 
   const onClickCreateButton = () => {
     navigate('/new');
@@ -13,7 +31,7 @@ const DiaryList = ({ monthlyDiaryList }) => {
   return (
     <div className="diary-list">
       <div className="menu-bar">
-        <select>
+        <select onChange={onChangeSortType}>
           <option value="latest">최신순</option>
           <option value="oldest">오래된 순</option>
         </select>
@@ -24,7 +42,7 @@ const DiaryList = ({ monthlyDiaryList }) => {
         />
       </div>
       <div className="list-wrapper">
-        {monthlyDiaryList.map((diary) => {
+        {sortedDiaryList.map((diary) => {
           return <DiaryItem key={diary.diaryId} {...diary} />;
         })}
       </div>
