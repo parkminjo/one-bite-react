dca9cb94237d878153082e848f39466efa9d6bcf
parkminjo <cruiser367@naver.com>
2025-05-21 11:22:28 +0900

    feat: 클릭했을 때 다른 페이지로 이동할 수 있도록 구현

--- section12/src/components/DiaryItem.jsx
+++ section12/src/components/DiaryItem.jsx
@@ -3,19 +3,32 @@ import { getEmotionImage } from '../utils/get-emotion-image';
 import Button from './Button';
 
 const DiaryItem = ({ diaryId, emotionId, createdDate, content }) => {
+  const navigate = useNavigate();
+
+  const onClickDiaryItem = () => {
+    navigate(`/diary/${diaryId}`);
+  };
+
+  const onClickUpdateButton = () => {
+    navigate(`/edit/${diaryId}`);
+  };
+
   return (
     <div className="diary-item">
-      <div className={`img-section img-section-${emotionId}`}>
+      <div
+        className={`img-section img-section-${emotionId}`}
+        onClick={onClickDiaryItem}
+      >
         <img src={getEmotionImage(emotionId)} alt="감정 이미지" />
       </div>
-      <div className="info-section">
+      <div className="info-section" onClick={onClickDiaryItem}>
         <div className="created-date">
           {new Date(createdDate).toLocaleDateString()}
         </div>
         <div className="content">{content}</div>
       </div>
       <div className="button-section">
-        <Button text="수정하기" />
+        <Button text="수정하기" onClick={onClickUpdateButton} />
       </div>
     </div>
   );
@@ -23,3 +36,4 @@ const DiaryItem = ({ diaryId, emotionId, createdDate, content }) => {
 
 export default DiaryItem;
 import './DiaryList.css';
+import { useNavigate } from 'react-router-dom';
