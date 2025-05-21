b7b6f4b9fefb89638dc198073acb363919edb2c7
parkminjo <cruiser367@naver.com>
2025-05-21 13:37:45 +0900

    feat: 일기 삭제하기 기능 연결 완료

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,9 +1,44 @@
-import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DiaryDispatchContext } from '../App';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
 
 const Edit = () => {
   const params = useParams();
+  const navigate = useNavigate();
 
-  return <div>{params.id}번 일기입니다.</div>;
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+
+  const onClickBackButton = () => {
+    navigate(-1);
+  };
+
+  const onClickDeleteButton = () => {
+    onDelete(params.id);
+  };
+
+  const onSubmit = () => {
+    onUpdate();
+  };
+
+  return (
+    <div>
+      <Header
+        title="일기 수정하기"
+        leftChild={<Button text="< 뒤로가기" onClick={onClickBackButton} />}
+        rightChild={
+          <Button
+            text="삭제하기"
+            type="negative"
+            onClick={onClickDeleteButton}
+          />
+        }
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Edit;
